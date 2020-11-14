d0295eee7fe8c444e2b2fa32298f5993b6f4dd1b
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-11-14 13:11:31 -0300

    feat: ensure storeInformationMovie returns 400 if id movie is not provided

---
+++ src/presentation/controllers/movie-controller.js
@@ -0,0 +1,14 @@
+class MovieController {
+  constructor(){
+
+  }
+
+  async storeInformationMovie(event) {
+  
+    return {
+      statusCode: 400
+    }
+  }
+}
+
+module.exports = MovieController
