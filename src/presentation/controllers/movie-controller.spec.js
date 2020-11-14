bcd86d034d81771cd61ec0872cac0d8dfb2d4b79
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-11-14 13:41:28 -0300

    test: add validator

--- src/presentation/controllers/movie-controller.spec.js
+++ src/presentation/controllers/movie-controller.spec.js
@@ -1,21 +1,47 @@
 const MovieController = require('./movie-controller')
+const { ValidatorError } = require('../errors')
 
 const makeSut = () => {
-  const sut = new MovieController()
+  const validatorStub = makeValidatorStub()
+  const sut = new MovieController(validatorStub)
 
   return {
-    sut
+    sut,
+    validatorStub
   }
+}
+
+const makeValidatorStub = () => {
+  class ValidatorStub {
+    validate(schema, eventBody) {
+      const validation = {
+        isValid: true,
+        err: {}
+      }
+
+      for(const each of schema) {
+        if(!eventBody[each.field]){
+          validation.isValid = false
+          validation.err[each.field] = 'Dados Inválidos' 
+        }
+      }
+
+      return validation
+    }
+  }
+
+  return new ValidatorStub()
 }
 
 describe('Movie Controller', () => {
   test('Should return 400 if id movie is not provided', async () => {
-    const {sut} = makeSut()
+    const { sut } = makeSut()
     const event = {
       body: {}
     }
 
     const res = await sut.storeInformationMovie(event)
     expect(res.statusCode).toBe(400)
+    expect(res.body.error).toBe(new ValidatorError({idMovie: 'Dados Inválidos'}).message)
   })
 })
