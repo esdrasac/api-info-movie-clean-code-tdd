8cc18774fb0233247c04890a1cce65bbfaa006f0
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-11-15 23:00:31 -0300

    test: ensure mongodb connect when client is disconnect

--- src/infra/helpers/mongo-db-helper.spec.js
+++ src/infra/helpers/mongo-db-helper.spec.js
@@ -9,12 +9,17 @@ describe('Mongo helper', () => {
   afterAll(async () => {
     await sut.disconnect()
   })
-
-  test('', async () => {
+  
+  test('Should reconnect when client is disconnect', async () => {
     expect(sut.client).toBeTruthy()
     await sut.disconnect()
     expect(sut.client).toBeFalsy()
     await sut.getOrCreateCollection('movies', movieSchema)
+    expect(sut.client).toBeTruthy()
+  })
+
+  test('Should connect when client is disconnect', async () => {
+    await sut.connect('mongodb://localhost:27017/api-info-movie')
     expect(sut.client).toBeTruthy()
   })
 })
